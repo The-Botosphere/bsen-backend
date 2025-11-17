80575f94201295286a9454520a215aaba3385432
myvfc <154605226+myvfc@users.noreply.github.com>
2025-11-16 22:34:59 -0600

    Update search.js

--- search.js
+++ search.js
@@ -3,17 +3,14 @@ import { embedText } from "./embeddings.js";
 
 export async function searchVideos(query) {
   const query_embedding = await embedText(query);
-
   const { data, error } = await supabase.rpc("match_videos", {
     query_embedding,
     match_threshold: 0.5,
     match_count: 10
   });
-
   if (error) {
     console.error("Supabase RPC Error:", error);
     throw error;
   }
-
   return data;
 }
