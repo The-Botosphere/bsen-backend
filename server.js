fe6c9baa63d7b23b9f278d9eccb6b1472ab5e9ee
myvfc <154605226+myvfc@users.noreply.github.com>
2025-11-16 19:31:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,6 @@ app.get("/load", async (req, res) => {
 
 // --- START SERVER ---
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 BSEN backend running on ${PORT}`);
 });
